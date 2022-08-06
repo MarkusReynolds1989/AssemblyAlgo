141244a1b0c0cf52c80943c0c58baf44ce7d486a
markus <markusreynolds1989@gmail.com>
2022-08-05 21:58:12 -0500

    Fixed algos

--- Main.asm
+++ Main.asm
@@ -7,66 +7,74 @@ include Irvine32.inc
 ExitProcess PROTO, dwExitCode:DWORD
 
 .data
-myList2 DWORD 3,8,3,10,110,5
-myList DWORD 1,2,3,4,5,6
-max DWORD -2147483647
-min DWORD 2147483647
-max_string BYTE "Max: ",0
-min_string BYTE "Min: ",0
+myList2 DWORD 3,8,3,10,110,5 ;; [i32, 6]
+myList DWORD 1,2,3,4,5,6 ;; [i32; 6]
+MIN DWORD -2147483647 ;; Const i32
+MAX DWORD 2147483647 ;; Const i32
 
 .code
 main PROC
-	mov ebp, 0; Array pointer.
-	mov ecx, 0 ; Set ecx as 0 for counting.
+	mov esi, OFFSET myList ; Point to myList.
+	mov ecx,LENGTHOF myList ; Counter is set to length of myList.
+	call GetMax
+	call WriteInt
 
-	compare_min_max: 
-
-		compare_for_max: 
-			mov eax, max
-			cmp eax, [myList + ebp] ; See if the max value is less than the current ebp.
-			jl set_max ; Set the max to be eax if it is more.
-
-		compare_for_min:
-			mov eax, min
-			cmp eax, [myList + ebp] 			
-			jg set_min
-
-			jmp increment
-	
-	set_max: 
-		mov eax, [myList + ebp]
-		mov max, eax	
-		
-		;; Go and see if this value is also less than the min.
-		jmp compare_for_min
-
-	set_min: 
-		mov eax, [myList + ebp]
-		mov min, eax  
-
-		jmp increment
-
-	;; Increment all the values.		
-	increment:
-		add ecx, 1
-		add ebp, 4
-		cmp ecx, 6
-		jl compare_min_max
-		je end_items
-	
-	end_items: 
-		call Crlf
-		mov edx,OFFSET max_string
-		call WriteString
-		mov eax, max
-		call WriteInt
-		call Crlf
-		mov eax, min
-		mov edx,OFFSET min_string
-		call WriteString
-		call WriteInt
+	call Crlf
+	mov esi, OFFSET myList2
+	mov ecx, LENGTHOF myList2
+	call GetMin
+	call WriteInt
 
 	INVOKE ExitProcess, 0
 main ENDP
+
+; Determines the max integer in an array.
+GetMax Proc
+
+	; We are doing this because both of these values will be updated in this procedure.
+	push esi ; Save esi, the source index register. This is used for copying the array.
+	push ecx ; Save ecx, this is the counter register. We are using this to track the length.
+
+	mov eax, MIN ; Set eax to the minimum value.
+
+	compare_for_max: 
+		cmp eax, [esi] ; Compare eax to the current array element.
+		jl set_max ;; If the current array element is greater than eax, set eax to that element.
+		jmp increment;; Otherwise increment to the next number.
+
+	set_max: 
+		mov eax, [esi] ; Push the array element to the stack.
+
+	increment:
+		add esi, TYPE DWORD ; Go to the next element of the array.
+		loop compare_for_max ; When we use loop it checks if we are still under the length of the array.
+		
+	pop ecx ; Reload the pervious ecx back into the register.
+	pop esi ; Reload the previous esi back into the register.
+	ret
+GetMax endp
+
+GetMin Proc
+	push esi
+	push ecx
+
+	mov eax,MAX
+
+	compare_for_min: 
+		cmp eax, [esi]
+		jg set_min
+		jmp increment
+
+	set_min: 
+		mov eax, [esi]
+
+	increment: 
+		add esi, TYPE DWORD
+		loop compare_for_min
+
+	pop ecx
+	pop esi
+	ret
+GetMin endp
 
 END main
